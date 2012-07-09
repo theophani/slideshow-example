60338575d9638bff6c2bf3873694027a72b99c8f
Tiffany Conroy <tiffany.conroy@gmail.com>
2012-07-09 21:45:40 +0200

    revert scroll function

--- javascripts/slideshow.js
+++ javascripts/slideshow.js
@@ -3,18 +3,3 @@ var filmroll = document.getElementById('the-filmroll');
 function setStyleAttr(element, attr, value) {
   element.style[attr] = value;
 }
-
-function scroll(element, from, to, speed) {
-  // speed is how many pixels to move every 10 milliseconds
-  var curr = from;
-
-  function move() {
-    var position = curr + 'px';
-    setStyleAttr(element, 'left', position);
-    curr = curr + speed;
-    if (curr >= to)
-      clearInterval(loop);
-  }
-
-  var loop = setInterval(move, 10);
-}
