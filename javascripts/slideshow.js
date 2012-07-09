71b2d58f32fe1243226f8bf42c97183402efe3a2
Tiffany Conroy <tiffany.conroy@gmail.com>
2012-07-09 21:14:19 +0200

    create a function for setting a style attribute

--- javascripts/slideshow.js
+++ javascripts/slideshow.js
@@ -1,1 +1,10 @@
 var filmroll = document.getElementById('the-filmroll');
+
+function setStyleAttr(element, attr, value) {
+  element.style[attr] = value;
+}
+
+/*
+Try:
+> setStyleAttr(filmroll, 'left', '-200px');
+*/
