52d8866876894cbfcb622d8dfc7871528a42d9ce
Tiffany Conroy <tiffany.conroy@gmail.com>
2012-07-09 21:46:38 +0200

    more convenient setLeft function

--- javascripts/slideshow.js
+++ javascripts/slideshow.js
@@ -3,3 +3,8 @@ var filmroll = document.getElementById('the-filmroll');
 function setStyleAttr(element, attr, value) {
   element.style[attr] = value;
 }
+
+function setLeft(element, pixels) {
+  var value = pixels + 'px';
+  setStyleAttr(element, 'left', value);
+}
