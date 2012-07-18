e716b5482e15461565dff9be81161b7b45209b9e
Tiffany Conroy <tiffany.conroy@gmail.com>
2012-07-18 09:41:21 +0200

    update to match instructions

---
+++ slideshow.js
@@ -0,0 +1,20 @@
+var filmroll = document.getElementById('the-film-roll');
+
+function scroll(start, end) {
+  var current = start;
+
+  var move = function () {
+    filmroll.style.left = current + 'px';
+
+    if (current == end)
+      clearInterval(loop);
+
+    if (end > start) {
+      current = Math.min(current + 20, end);
+    } else {
+      current = Math.max(current - 20, end);
+    }
+  }
+
+  var loop = setInterval(move, 50);
+}
